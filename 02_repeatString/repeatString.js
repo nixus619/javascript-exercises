9b23636e474e71702b638271a1fff89704a3fb5e
nixus619 <nickdcedor@gmail.com>
2024-06-21 16:07:30 -0400

    Completed 1-3
    Will try to finish 4-7 tomorrow or later today

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num==0){
+        return "";
+    } else if (num<0) {
+        return "ERROR";
+    } else {
+        let final = string;
+        for(let i = 1;i<num;i++) {
+            final = final + string;
+        }
+        return final;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strin) {
+    const array = strin.split("");
+    let finalString = "";
+    for (i=array.length - 1; i>=0; i--) {
+        finalString = finalString + array[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
