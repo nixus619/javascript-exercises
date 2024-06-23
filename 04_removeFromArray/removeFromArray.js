006f20f49c2910347be0c1c24e0054f5d3e56545
nixus619 <nickdcedor@gmail.com>
2024-06-23 17:18:54 -0400

    Finished exercises 4 and 5
    Will do 6 and 7 tomorrow

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    args.forEach((arg) => {
+        for(i=array.length-1;i>=0;i--) {
+            if((arg===array[i])) {
+                array.splice(i,1);
+            }
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum=0;
+    if(num1<0 || num2<0) {
+        return "ERROR";
+    }
+    if(typeof(num1)!= 'number' || typeof(num2) != 'number'){
+        return "ERROR";
+    }
+    if(num2>num1){
+        while(num2!=num1){
+            sum = sum + num2;
+            num2--;
+        }
+        sum = sum + num1
+    } else {
+        while(num1!=num2){
+            sum = sum + num1;
+            num1--;
+        }
+        sum = sum + num2;
+    }
+    return sum;
 };
 
 // Do not edit below this line
