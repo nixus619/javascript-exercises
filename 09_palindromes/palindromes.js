53cdc16268a2ca2a513b133721aae2a6cbe0184b
nixus619 <nickdcedor@gmail.com>
2024-07-06 18:17:53 -0400

    Got 09-Palindromes all passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let removePunc =  string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let removeSpaces = removePunc.replace(/\s/g, '');
+    removeSpaces = removeSpaces.toLowerCase();
+    console.log(removeSpaces);
+    let array = removeSpaces.split("");
+    let reverse = array.reverse();
+    let finalStr = reverse.join("");
+    if(removeSpaces === finalStr){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
