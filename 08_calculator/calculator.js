2641fb2f9c9909f9362bf835c3ade44701190c71
nixus619 <nickdcedor@gmail.com>
2024-07-05 21:37:26 -0400

    Finished javascript-exercises 08-calculators

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum1 = array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return sum1;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const mult = array.reduce((total,currentItem) => {
+    return total * currentItem;
+  },1);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number == 0){
+    return 1;
+  }
+  let count = number;
+  while(number !=0) {
+    if ((number - 1) ==0) {
+      number = number - 1;
+    }
+    else {
+      count = count * (number - 1);
+      number = number - 1;
+    }
+  }
+	return count;
 };
 
 // Do not edit below this line
