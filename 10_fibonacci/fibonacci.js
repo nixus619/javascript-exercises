be28aae99ee3515eb574370157800c3b989f1ab8
nixus619 <nickdcedor@gmail.com>
2024-07-06 21:43:56 -0400

    Compelted fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(parseInt(number)<0){
+        return "OOPS";
+    }
+    let newNumber = parseInt(number);
+    let original = [0,1];
+    for(let i=1;i<newNumber;i++){
+        original.   push(original[i]+original[i-1]);
+    }
+    return original[newNumber];
 
 };
 
