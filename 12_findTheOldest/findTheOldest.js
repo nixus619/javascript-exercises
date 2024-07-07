b9ff90ae745f39eb81865178b40cd534120a420f
nixus619 <nickdcedor@gmail.com>
2024-07-07 15:06:43 -0400

    Finished exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentOldest = 0;
+    let oldestPerson;
+    let currentAge;
+    const d = new Date()
+    for(let person in people){
+        if(people[person].yearOfDeath===undefined)
+            {
+                currentAge = d.getFullYear() - people[person].yearOfBirth;
+            } else {
+                currentAge = people[person].yearOfDeath - people[person].yearOfBirth;
+            }
+        if(currentAge > currentOldest){
+            oldestPerson=people[person];
+            currentOldest=currentAge
+        }
+    }
+    return oldestPerson;
+    /*const titles = []
+    for(let item in items){
+       titles.push(items[item].title); 
+    }
+    return titles;*/
+    // More elegant:
+    return items.map((item) => item.title);
 
 };
 
