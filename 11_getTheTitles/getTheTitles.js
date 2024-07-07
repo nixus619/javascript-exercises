ca33ee819c77db9bb22426a7aef1f03897578a9f
nixus619 <nickdcedor@gmail.com>
2024-07-07 09:15:33 -0400

    Finished 11_getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(items) {
+    /*const titles = []
+    for(let item in items){
+       titles.push(items[item].title); 
+    }
+    return titles;*/
+    // More elegant:
+    return items.map((item) => item.title);
 
 };
 
